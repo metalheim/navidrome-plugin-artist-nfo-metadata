bc61476c887786d841ec42915b19a4359ba59e48
Stephan Wahlen <stephan.wahlen@agheera.com>
2026-02-04 17:42:49 +0100

    remove custom build script

--- build-ndp.sh
+++
@@ -1,5 +0,0 @@
-go mod tidy
-tinygo build -o plugin.wasm -target wasip1 -buildmode=c-shared .
-zip -j artist-nfo-metadata.ndp manifest.json plugin.wasm
-rm -f plugin.wasm
-cp -f artist-nfo-metadata.ndp ../navidrome/plugins
